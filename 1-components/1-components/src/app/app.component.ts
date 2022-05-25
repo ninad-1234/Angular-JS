92d201aa9c393ab249c073dc2bd8971d0cfc6f52
ninad-1234 <topaleninad01@gmail.com>
2022-05-25 17:29:42 +0530

    AFter_view_init_Lifecycle

--- 1-components/1-components/src/app/app.component.ts
+++ 1-components/1-components/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import {COURSES} from '../db-data';
 import { CourseCardComponent } from './course-card/course-card.component';
 import { Course } from './model/course';
@@ -8,18 +8,32 @@ import { Course } from './model/course';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 
   courses = COURSES;
 
   @ViewChild('cardRef1',{read: ElementRef})
   card1 :CourseCardComponent;
 
-  @ViewChild('cardRef2')
-  card2 :CourseCardComponent;
+  // @ViewChild('cardRef2')
+  // card2 :CourseCardComponent;
   
-  @ViewChild('container')
-  containerDiv :ElementRef;
+  @ViewChild('courseImage')
+  courseImage :ElementRef;
+
+  constructor(){
+    
+  }
+  ngAfterViewInit() {
+
+    console.log("courseImage",this.courseImage);
+
+   // this.courses[0].description = 'test'
+
+  }
+  onCourseSelected(course:Course){
+    // console.log("containerDiv",this.containerDiv);
+  }
 
   // title = COURSES[0].description;
 
@@ -31,9 +45,7 @@ export class AppComponent {
 
   // startDate = new Date(2000, 0, 1);
 
-  onCourseSelected(course:Course){
-    console.log("containerDiv",this.containerDiv);
-  }
+  
 
 
 
