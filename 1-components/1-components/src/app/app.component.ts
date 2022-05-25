6bcc8e609855142d9d53544a9d369f2ec9430ec2
ninad-1234 <topaleninad01@gmail.com>
2022-05-25 10:57:57 +0530

    ngclass functionality

---
+++ 1-components/1-components/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import {COURSES} from '../db-data';
+import { Course } from './model/course';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  courses = COURSES;
+
+  onCourseSelected(course:Course){
+    console.log("App component - click event bubbled ...", course);
+  }
+
+
+
+}
---
+++ 1-components/1-components/src/app/course-card/course-card.component.ts
@@ -0,0 +1,42 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {COURSES} from '../../db-data'
+import {Course} from '../model/course'
+
+
+@Component({
+  selector: 'course-card',
+  templateUrl: './course-card.component.html',
+  styleUrls: ['./course-card.component.css']
+})
+export class CourseCardComponent implements OnInit {
+
+  @Input()
+  course:Course;
+
+  @Input()
+  cardIndex:Number;
+
+  @Output('courseSelected')
+  courseEmitter = new EventEmitter<Course>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  isImageVisible(){
+    return this.course && this.course.iconUrl;
+  }
+
+  onCourseViewed(){
+    console.log("card component - button clicked ...");
+    this.courseEmitter.emit(this.course);
+  }
+
+  cardClasses(){
+    if (this.course.category == 'BEGINNER'){
+      return 'beginner';
+    }
+  }
+
+}
