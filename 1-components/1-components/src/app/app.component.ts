20ac635acecd475161bb4ed1b2f61f369d21e0be
ninad-1234 <topaleninad01@gmail.com>
2022-05-25 13:10:00 +0530

    angular-pipe-functionality

--- 1-components/1-components/src/app/app.component.ts
+++ 1-components/1-components/src/app/app.component.ts
@@ -11,6 +11,16 @@ export class AppComponent {
 
   courses = COURSES;
 
+  title = COURSES[0].description;
+
+  price = 9.95863326698;
+
+  rate = 0.67 ;
+
+  course = COURSES[0];
+
+  startDate = new Date(2000, 0, 1);
+
   onCourseSelected(course:Course){
     console.log("App component - click event bubbled ...", course);
   }
